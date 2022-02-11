bd5bc77500635a5349e039e9a3ec52066440d889
Howard Lau <howardlau1999@hotmail.com>
2022-02-11 22:30:28 +0800

    *: rename

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# AutograderFe
+# AutograderWeb
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 13.1.2.
 
--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/autograder-fe'),
+      dir: require('path').join(__dirname, './coverage/autograder-web'),
       subdir: '.',
       reporters: [
         { type: 'html' },
